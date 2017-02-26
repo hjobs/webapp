03119e777e0ae2c72570db30b223d7f6c646a8bd
Edmund <edmundtfy@gmail.com>
2017-02-26 16:25:18 +0800

    supported 'quick' instead of 'casual', and 'orgs' instead of 'org'

--- src/var.js
+++ src/var.js
@@ -1,8 +1,8 @@
 class Variable {
   constructor() {
     // this.baseUrl = "http://api.hjobs.hk:9080/";
-    this.baseUrl = "http://dev.hjobs.hk:9080/";
-    // this.baseUrl = "http://localhost:9080/";
+    // this.baseUrl = "http://dev.hjobs.hk:9080/";
+    this.baseUrl = "http://localhost:9080/";
   }
 }
 
