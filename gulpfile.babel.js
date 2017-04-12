1581f60d97be04e655038b00c8378b0fe7479eed
Edmund <edmundtfy@gmail.com>
2017-04-12 23:21:28 +0800

    built with newer version number

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -120,9 +120,9 @@ gulp.task('fontAwesome', () => {
 gulp.task('htmlReplace', () => {
   gulp.src('index.html')
   .pipe(htmlReplace({
-    css: './styles/main.css?version=20170410v1',
+    css: './styles/main.css?version=20170412v1',
     // bootstrap: 'styles/bootstrap.css',
-    js: './js/app.js?version=20170410v1',
+    js: './js/app.js?version=20170412v1',
     browserSync: '' }))
   .pipe(gulp.dest(paths.dist));
 });
