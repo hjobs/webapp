680b2d2f20dffa2ba990532e9f9c021a317fa778
Edmund <edmundtfy@gmail.com>
2017-05-22 13:15:09 +0800

    sharing component, fixing facebook

--- src/Components/Job/ShareButtons.js
+++ src/Components/Job/ShareButtons.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Reflux from 'reflux';
 import { ShareButtons, generateShareIcon } from 'react-share';
-const { FacebookShareButton, LinkedinShareButton, TwitterShareButton, WhatsappShareButton } = ShareButtons;
+const { LinkedinShareButton, TwitterShareButton, WhatsappShareButton } = ShareButtons;
 const socialButtons = [
-  {type: FacebookShareButton, key: "facebook"},
+  // {type: FacebookShareButton, key: "facebook"},
   {type: LinkedinShareButton, key: "linkedin"},
   {type: TwitterShareButton, key: "twitter"},
   {type: WhatsappShareButton, key: "whatsapp"},
 ]
+const FacebookIcon = generateShareIcon('facebook');
 import CopyToClipboard from 'react-copy-to-clipboard';
 import './styles/ShareButtons.css'
 
@@ -51,14 +52,27 @@ export default class ActionButtons extends Reflux.Component {
           link = "https://www.hjobs.hk/jobs/stable?job=" + job.id;
 
     return (
-      <div className="flex-row flex-hCenter flex-vEnd" style={this.props.style}>
+      <div className="flex-row flex-hCenter" style={this.props.style}>
         <div>
           {t.shareIn}:
+        </div>
+        <div className="padded-sides" onClick={() => log(this.shareButtonLog("facebook"))}>
+          <div
+            className="fb-share-button"
+            data-href="https://hjobs.hk"
+            data-layout="button" data-size="small" data-mobile-iframe="true">
+            <a
+              className="fb-xfbml-parse-ignore" target="_blank"
+              href={"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.hjobs.hk%2Fjobs%2Fstable%3Fjob%3D" + job.id + "&amp;src=sdkpreparse"}>
+              <FacebookIcon round size={22} />
+            </a>
+          </div>
         </div>
         {
           socialButtons.map((item, i) => React.createElement(
             "div",
             {
+              style: {marginTop: -3},
               key: "socialbuttons-" + item.key,
               onClick: () => log(this.shareButtonLog(item.key))
             },
