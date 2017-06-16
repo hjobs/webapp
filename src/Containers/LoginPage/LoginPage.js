8d549d1bd511c3e1d782e3f055c75bf5f8969f8a
Edmund <edmundtfy@gmail.com>
2017-06-16 17:49:26 +0800

    built for dev, login page button points to admin page

--- src/Containers/LoginPage/LoginPage.js
+++ src/Containers/LoginPage/LoginPage.js
@@ -8,21 +8,48 @@ import './LoginPage.css';
 import Login from '../../Components/Login/Login';
 
 import UserStore from '../../stores/userStore';
+import TranslationStore from '../../stores/translationStore';
 
 class LoginPage extends Reflux.Component {
   constructor(props) {
     super(props);
-    this.store = UserStore;
-    this.storeKeys = ["user", "authToken"];
+    this.stores = [UserStore, TranslationStore];
+    this.storeKeys = ["user", "authToken", "locale"];
+    this.tStrings = {
+      "en": {
+        postJob: "Post job? Click here!"
+      },
+      "zh-HK": {
+        postJob: "想刑登工作？按這裡!"
+      }
+    }
   }
 
   render() {
     if (!!this.state.authToken && !!this.state.user) return <Redirect to="/jobs/stable" />;
+    const tStrings = this.tStrings[this.state.locale];
+
     return (
       <div className="flex-col flex-vhCenter login-page-container">
         <div className="login-container" style={{fontSize: "16px"}}>
           <Login />
         </div>
+        <a
+          href="https://admin.hjobs.hk"
+          target="_blank"
+        >
+          <button
+            style={{
+              padding: 12,
+              border: 0,
+              borderRadius: 3,
+              color: "black",
+              fontSize: 16
+            }}
+          >
+            {tStrings.postJob}
+          </button>
+        </a>
       </div>
     );
   }
