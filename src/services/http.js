e737fde4c286483de5955cb8d2491960e630ba8c
Edmund <edmundtfy@gmail.com>
2017-12-08 20:49:25 +0800

    stupid http.js baseUrl syntax mistake

--- src/services/http.js
+++ src/services/http.js
@@ -6,9 +6,9 @@ import 'whatwg-fetch';
 const getToken = () => (localStorage.getItem("authToken") || null);
 
 // export const baseUrl = process.env.REACT_APP_BASEURL;
-export const baseUrl = "https://api.hjobs.hk";
-// export const baseUrl = "https://dev.hjobs.hk";
-// export const baseUrl = "http://localhost:8080";
+// export const baseUrl = "https://api.hjobs.hk/";
+export const baseUrl = "https://dev.hjobs.hk/";
+// export const baseUrl = "http://localhost:8080/";
 
 /** param httpMethod defaults to 'GET', data defaults to null
 * @param {string} urlSuffix
